0d4279d886cdbe396e9daf54a9d484e186f4afa3
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2024-06-20 23:11:36 +0530

    some changes to imporve performance

--- config/database.js
+++ config/database.js
@@ -1,9 +1,16 @@
 let mongoose = require('mongoose');
 
-let local_db = process.env.REMOTE_URL ||  `mongodb://localhost:27017/webflix`;
+let local_db = process.env.REMOTE_URL;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  poolSize: 10
+}
+
 
 let dbConnect = function(){ 
-  return mongoose.connect(local_db)
+  return mongoose.connect(local_db,options)
   .then(res=>console.log('db is connected'))
   .catch(err=>console.log(err))
 }
